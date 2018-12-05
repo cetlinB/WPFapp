09462407b72b7daaae85512af511772e3b022745
cetlinsb <cetlinskib@gmail.com>
2018-12-05 10:52:34 +0100

    Renamed folders and recreated project.

---
+++ NoteClassLibrary/Model/Address.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleClassLibrary.BaseClass;
+using System.Xml.Serialization;
+
+namespace SampleClassLibrary.Model
+{
+    public class Address : NotificationObject
+    {
+        string street;
+
+        [XmlElement(ElementName = "Street")]
+        public string Street
+        {
+            get { return this.street; }
+            set
+            {
+                if (this.street != value)
+                {
+                    this.street = value;
+                    OnPropertyChanged("Street");
+                }
+            }
+        }
+        string town;
+
+        [XmlElement(ElementName = "Town")]
+        public string Town
+        {
+            get { return this.town; }
+            set
+            {
+                if (this.town != value)
+                {
+                    this.town = value;
+                    OnPropertyChanged("Town");
+                }
+            }
+        }
+        string province;
+
+        [XmlElement(ElementName = "Province")]
+        public string Province
+        {
+            get { return this.province; }
+            set
+            {
+                if (this.province != value)
+                {
+                    this.province = value;
+                    OnPropertyChanged("Province");
+                }
+            }
+        }
+        string zipcode;
+
+        [XmlElement(ElementName = "Zipcode")]
+        public string Zipcode
+        {
+            get { return this.zipcode; }
+            set
+            {
+                if (this.zipcode != value)
+                {
+                    this.zipcode = value;
+                    OnPropertyChanged("Zipcode");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Street:\t{0}\nTown:\t{1}\nProvince:\t{2}\nZipcode:\t{3}", this.Street, this.Town, this.Province, this.Zipcode);
+        }
+    }
+}
---
+++ NoteClassLibrary/Model/Entry.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleClassLibrary.BaseClass;
+using System.Xml.Serialization;
+
+namespace SampleClassLibrary.Model
+{
+    public class Entry : NotificationObject
+    {
+        string name;
+
+        [XmlElement(ElementName = "Name")]
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        string surname;
+
+        [XmlElement(ElementName = "Surname")]
+        public string Surname
+        {
+            get { return this.surname; }
+            set
+            {
+                if (this.surname != value)
+                {
+                    this.surname = value;
+                    OnPropertyChanged("Surname");
+                }
+            }
+        }
+        string company;
+
+        [XmlElement(ElementName = "Company")]
+        public string Company
+        {
+            get { return this.company; }
+            set
+            {
+                if (this.company != value)
+                {
+                    this.company = value;
+                    OnPropertyChanged("Company");
+                }
+            }
+        }
+
+        [XmlElement(ElementName = "Telephone")]
+        ObservableCollection<Telephone> telephones;
+        public ObservableCollection<Telephone> Telephones
+        {
+            get
+            { return this.telephones; }
+            set
+            {
+                if (this.telephones != value)
+                {
+                    this.telephones = value;
+                    OnPropertyChanged("Telephones");
+                }
+            }
+        }
+
+        [XmlElement(ElementName = "Address")]
+        ObservableCollection<Address> addresses;
+        public ObservableCollection<Address> Addresses
+        {
+            get
+            { return this.addresses; }
+            set
+            {
+                if (this.addresses != value)
+                {
+                    this.addresses = value;
+                    OnPropertyChanged("Telephones");
+                }
+            }
+        }
+
+        public Entry()
+        {
+            this.Telephones = new ObservableCollection<Telephone>();
+            this.Addresses = new ObservableCollection<Address>();
+        }
+    }
+}
