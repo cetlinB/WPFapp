ea498843ee6c973a8911632553c8d61cd79913d6
Bartek <cetlinskib@gmail.com>
2019-01-16 00:39:36 +0100

    DataGrid calendar first revison.

--- NoteProject/CalendarWindow.xaml.cs
+++ NoteProject/CalendarWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -21,16 +22,40 @@ namespace NoteProject
     public partial class CalendarWindow : Window
     {
         private DateTime current;
+        private DataTable month;
         public CalendarWindow()
         {
             current = DateTime.Now;
+            InitializeDaysTable();
             InitializeComponent();
+            FillDaysGrid();
             MonthName.Text = GetMonthName(current.Month) + " " + current.Year.ToString();
+        }
+
+        private void InitializeDaysTable()
+        {
+            month = new DataTable();
+            for (int day = 0; day < 7; day++)
+                month.Columns.Add(((DayOfWeek)day).ToString(),typeof(string));
         }
 
         private void FillDaysGrid()
         {
-            //DaysOfTheMonth
+            int dayOfMonth = 0;
+            for(int week = 0; week < 5; week++)
+            {
+                DataRow temp = month.NewRow();
+                for(int day = 0; day < 7; day++)
+                {
+                    if(week*7+day <= DateTime.DaysInMonth(current.Year,current.Month))
+                        temp[day] = ++dayOfMonth;
+                }
+                month.Rows.Add(temp);
+            }
+            //TODO wstawienie tabeli do widoku
+
+            DaysOfTheMonth.CurrentItem = month;
+
         }
 
         private void NextMonth(object sender, RoutedEventArgs e)
